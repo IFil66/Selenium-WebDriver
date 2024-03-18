cc73a4f818871375dc75727e2caa32699bc00305
ruafli5 <ivan.filimonov@raiffeisen.ru>
2024-03-18 17:04:12 +0300

    add success authorization admin panel tests

--- src/test/java/AdminAuthorizationPageTest.java
+++ src/test/java/AdminAuthorizationPageTest.java
@@ -10,13 +10,17 @@ import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AdminAuthorizationPageTest extends TestBase {
-    final String pageUrl = "http://localhost/litecart/admin/";
+    final String pageUrl = "http://localhost:8080/litecart/admin/";
+    final String adminLogin = "admin";
+    final String adminPassword = "admin";
+
     final String locatorOfUserNameText = "div.content tbody tr:nth-child(1) td:nth-child(1)";
     final String locatorOfUserNameField = "div.content tbody tr:nth-child(1) td:nth-child(2) input";
     final String locatorOfPasswordText = "div.content tbody tr:nth-child(2) td:nth-child(1)";
     final String locatorOfPasswordField = "div.content tbody tr:nth-child(2) td:nth-child(2) input";
     final String locatorOfLoginButton = "button[name='login']";
-    final String locatorOfErrorNoticeText = "#notices div";
+    final String locatorOfErrorNoticeText = ".notice.errors";
+    final String locatorOfSuccessNoticeText = ".notice.success";
 
     Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(60));
 
@@ -37,12 +41,29 @@ public class AdminAuthorizationPageTest extends TestBase {
 
     @Test
     @DisplayName("Проверка текста уведомления об ошибке при авторизации с невалидными данными")
+    public void checkSuccessNoticeTextWhenSuccessfulAuthorizationToAdminPanelTest() {
+        driver.navigate().to(pageUrl);
+
+        driver.findElement(By.cssSelector(locatorOfUserNameField)).sendKeys(adminLogin);
+        driver.findElement(By.cssSelector(locatorOfPasswordField)).sendKeys(adminPassword);
+        driver.findElement(By.cssSelector(locatorOfLoginButton)).click();
+
+        WebElement successNoticeElement = driver.findElement(By.cssSelector(locatorOfSuccessNoticeText));
+        wait.until(e -> successNoticeElement.isDisplayed());
+        String actualErrorNoticeText = successNoticeElement.getText();
+
+        Assertions.assertEquals("You are now logged in as admin", actualErrorNoticeText);
+    }
+
+    @Test
+    @DisplayName("Проверка текста уведомления об ошибке при авторизации с невалидными данными")
     public void checkErrorNoticeTextWhenUnsuccessfulAuthorizationToAdminPanelTest() {
         driver.navigate().to(pageUrl);
 
         driver.findElement(By.cssSelector(locatorOfUserNameField)).sendKeys("123");
         driver.findElement(By.cssSelector(locatorOfPasswordField)).sendKeys("123");
         driver.findElement(By.cssSelector(locatorOfLoginButton)).click();
+
         WebElement errorNoticeElement = driver.findElement(By.cssSelector(locatorOfErrorNoticeText));
         wait.until(e -> errorNoticeElement.isDisplayed());
         String actualErrorNoticeText = errorNoticeElement.getText();
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -4,14 +4,33 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 public class TestBase {
     public WebDriver driver;
 
     @BeforeEach
     public void start() {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//        ChromeOptions options = new ChromeOptions();
+//        driver = new ChromeDriver(options);
+
+        FirefoxOptions options = new FirefoxOptions();
+        driver = new FirefoxDriver(options);
+
+//        SafariOptions options = new SafariOptions();
+//        driver = new SafariDriver(options);
+
+//        EdgeOptions options = new EdgeOptions();
+//        driver = new EdgeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
     }
 
     @AfterEach
