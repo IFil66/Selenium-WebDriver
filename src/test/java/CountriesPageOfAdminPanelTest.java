d7f73607284684b5fa265fd6412c748f4fc520b4
ruafli5 <ivan.filimonov@raiffeisen.ru>
2024-03-28 15:01:15 +0300

    add test of check zone names sorted alphabetically

--- src/test/java/CountriesPageOfAdminPanelTest.java
+++ src/test/java/CountriesPageOfAdminPanelTest.java
@@ -10,13 +10,17 @@ import static org.openqa.selenium.By.cssSelector;
 public class CountriesPageOfAdminPanelTest extends TestBase {
     final AdminAuthorizationPageTest adminAuthorizationPage = new AdminAuthorizationPageTest();
     final String pageUrl = "http://localhost/litecart/admin/?app=countries&doc=countries";
-    final String locatorOfAllLinksWithCountryName = ".row td:nth-child(5) a";
+    final String allRowCountryNameTable = ".row";
+    final String allLinksWithCountryName = ".row td:nth-child(5) a";
+    final String countryName = "td:nth-child(5) a";
+    final String countOfZone = "td:nth-child(6)";
+    final String allZonesOfCountry = "#table-zones tr:not(:last-child) td:nth-child(3)";
 
     @Test
     @DisplayName("Проверка сортировки названий стран по алфавиту")
     public void getCountryNamesListAndCheckIfItIsSortedAlphabetically() {
         adminAuthorizationPage.goToAdminPageAndLoginThere(driver, pageUrl);
-        List<String> countryNamesList = driver.findElements(cssSelector(locatorOfAllLinksWithCountryName)).stream()
+        List<String> countryNamesList = driver.findElements(cssSelector(allLinksWithCountryName)).stream()
                 .map(WebElement::getText)
                 .toList();
         List<String> sortedCountryNamesList = countryNamesList.stream()
@@ -25,6 +29,30 @@ public class CountriesPageOfAdminPanelTest extends TestBase {
         for (int i = 0; i < countryNamesList.size(); i++) {
             Assertions.assertEquals(sortedCountryNamesList.get(i), countryNamesList.get(i),
                     "The list of countries name is not sorted alphabetically");
+        }
+    }
+
+    @Test
+    @DisplayName("Проверка сортировки геозон в странах по алфавиту")
+    public void getZoneNamesListAndCheckIfItIsSortedAlphabetically() {
+        adminAuthorizationPage.goToAdminPageAndLoginThere(driver, pageUrl);
+        List<WebElement> webElements = driver.findElements(cssSelector(allRowCountryNameTable));
+        List<String> countryLinkList = webElements.stream()
+                .filter(element -> Integer.parseInt(element.findElement(cssSelector(countOfZone)).getText()) > 0)
+                .map(element -> element.findElement(cssSelector(countryName)).getAttribute("href"))
+                .toList();
+        for (String link : countryLinkList) {
+            driver.navigate().to(link);
+            List<String> zoneNameList = driver.findElements(cssSelector(allZonesOfCountry)).stream()
+                    .map(WebElement::getText)
+                    .toList();
+            List<String> sortedZoneNameList = zoneNameList.stream()
+                    .sorted()
+                    .toList();
+            for (int i = 0; i < zoneNameList.size(); i++) {
+                Assertions.assertEquals(sortedZoneNameList.get(i), zoneNameList.get(i),
+                        "The list of zone name is not sorted alphabetically");
+            }
         }
     }
 }
